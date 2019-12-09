3ef557f06547301e38b5edb0e29c36005a037d73
allanherm <allan.hermann@gmail.com>
2019-12-09 00:12:23 -0400

    Added Relationships Insertion

--- app.py
+++ app.py
@@ -24,7 +24,9 @@ class Escola(db.Model):
     endereco = db.Column(db.String(90))
     situacao = db.Column(db.String(45), nullable=True)
     data = db.Column(db.Integer, nullable=True)
-    turmas = db.relationship("Turma", backref="escola", cascade="all, delete-orphan", lazy="dynamic")
+    turmas = db.relationship(
+        "Turma", backref="escola", lazy="dynamic"
+    )
 
     def __init__(self, nome, endereco, situacao, data):
         self.nome = nome
@@ -57,7 +59,10 @@ class Aluno(db.Model):
     nascimento = db.Column(db.Date)
     genero = db.Column(db.String(1))
     aulas = db.relationship(
-        "Turma", secondary=aulas, backref=db.backref("estudantes", cascade="all, delete-orphan"), lazy="dynamic"
+        "Turma",
+        secondary=aulas,
+        backref=db.backref("estudantes"),
+        lazy="dynamic",
     )
 
     def __init__(self, nome, telefone, email, nascimento, genero):
@@ -284,7 +289,7 @@ def aulaselecionar():
 
 @app.route("/formselect/<int:id>", methods=["GET", "POST"])
 def selectform(id):
-    if request.method=="POST":
+    if request.method == "POST":
         aluno = Aluno.query.get_or_404(id)
         todasaulas = request.form.getlist("class")
         for aula in todasaulas:
